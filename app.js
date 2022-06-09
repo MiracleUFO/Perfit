5ed1faa5f85b988a3a3071adc41ebc7c44262a84
Miracle Ufodiama <miracleufodiama@gmail.com>
2022-06-09 22:30:51 +0100

    feat: added routes

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const path = require("path")
-require("dotenv").config({ path: "./config.env" });
+const router = require('./routers/router');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config({ path: './config.env' });
 
 const port = process.env.PORT || 6000;
+const db = require('./db/connection');
+const seedDatabase = require('./services/seeder');
 
-const db = require("./db/connection");
-const seedDatabase = require("./services/seeder");
-
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(cors());
 app.use(express.json());
+app.use('/api/', router);
 
-app.get("*", (res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+app.get('*', (res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
  
 app.listen(port, () => {
--- routers/router.js
+++ routers/router.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const { getUsers, getUser, editExistingUser, addNewUser } = require('../controllers/usersController');
 
 const router = express.Router();
 
 router.get('users', getUsers);
 
-router.get('user')
+router.get('users/:id', getUser);
+
+router.put('users/:id', editExistingUser);
+
+router.post('users', addNewUser);
+
+modules.exports = router;
