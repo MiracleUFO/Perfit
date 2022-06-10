483a5f88d624a56e90a39627e6bf081bccf27efc
Miracle Ufodiama <miracleufodiama@gmail.com>
2022-06-10 14:53:09 +0100

    chore: changes for heroku deploy

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const router = require('./routers/router');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config({ path: './config.env' });
 
 const port = process.env.PORT || 8080;
@@ -12,10 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/', router);
-
-app.use((req, res, next) => {
-    next('404 RouteNotFound. This is the end of the earth.');
-});
  
 app.listen(port, () => {
   if (db) {
@@ -24,10 +21,14 @@ app.listen(port, () => {
 });
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.get('*', function (req, res) {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
- }
+}
+
+app.use((req, res, next) => {
+  next('404 RouteNotFound. This is the end of the earth.');
+});
 
 module.exports = app;
