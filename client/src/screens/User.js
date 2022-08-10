0a965e9d74a0159cc2ca59c3087580e0f78fba40
Miracle Ufodiama <miracleufodiama@gmail.com>
2022-08-10 21:42:31 +0100

    fix: spacing in User component

--- client/src/screens/User.js
+++ client/src/screens/User.js
@@ -46,87 +46,83 @@ const User = () => {
         } else setLoading(false);
     },  [id]);
 
-    if (loading) {
-        return (
+    if (loading) return (
             <div className='loader-container-larger'>
                 <Loader />
             </div>
-        );
-    } else if (!id && id !== 0) {
-       return (
+        ); else if (!id && id !== 0) return (
             <Error msg='User not specified'>
                 <NavLink to='/'>Go to Home</NavLink>
             </Error>
-       );
-    } else return (
-        <main>
-            <AddProfileModal />
-            <EditProfileModal 
-                id={id}
-                name={userInfo.firstName}
-            />
-                <div>
-                    <Header />
-                    <div className='user-info'>
-                        <header></header>
-                        <div className='content'>
-                            <section className='content-intro'>
-                                <div className='avatar-container'>
-                                    <div className='avatar-container-small'>
-                                        <img 
-                                            className='avatar animate__animated animate__fadeInRight animate__fast'
-                                            src={userInfo.profilePicture}
-                                            alt='User&apos;s avatar'
-                                        />
-                                        <TbEditCircle 
-                                            className='edit-icon' 
+        ); else return (
+                <main>
+                    <AddProfileModal />
+                    <EditProfileModal 
+                        id={id}
+                        name={userInfo.firstName}
+                    />
+                    <div>
+                        <Header />
+                        <div className='user-info'>
+                            <header></header>
+                            <div className='content'>
+                                <section className='content-intro'>
+                                    <div className='avatar-container'>
+                                        <div className='avatar-container-small'>
+                                            <img 
+                                                className='avatar animate__animated animate__fadeInRight animate__fast'
+                                                src={userInfo.profilePicture}
+                                                alt='User&apos;s avatar'
+                                            />
+                                            <TbEditCircle 
+                                                className='edit-icon' 
+                                                onClick={displayEditProfileModal}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className='user-name-container'>
+                                        <div className='animate__animated animate__fadeInRight'>
+                                            <h1>{userInfo.firstName} {userInfo.lastName}</h1>
+                                            <p>I&apos;m a(n) {userInfo.occupation} based in {userInfo.state}.</p>
+                                        </div>
+                                        <div 
+                                            className='edit-box animate__animated animate__fadeInUp'
                                             onClick={displayEditProfileModal}
-                                        />
+                                        >
+                                            Edit
+                                        </div>
                                     </div>
-                                </div>
+                                </section>
 
-                                <div className='user-name-container'>
-                                    <div className='animate__animated animate__fadeInRight'>
-                                        <h1>{userInfo.firstName} {userInfo.lastName}</h1>
-                                        <p>I&apos;m a(n) {userInfo.occupation} based in {userInfo.state}.</p>
+                                <section className='about'>
+                                    <div className='animate__animated animate__fadeInUp'>
+                                        <h2>About Me</h2>
+                                        <p>
+                                            My name is {userInfo.firstName} {userInfo.lastName}.
+                                            I&apos;m a(n) {userInfo.occupation} based in {userInfo.state}, {userInfo.country}.
+                                        </p>
+                                        <p>
+                                            I specialise in {userInfo.keySkill}.
+                                            <span className='more'>..</span>
+                                        </p>
+                                        <span>Hire me</span>
                                     </div>
-                                    <div 
-                                        className='edit-box animate__animated animate__fadeInUp'
-                                        onClick={displayEditProfileModal}
-                                    >
-                                        Edit
+
+                                    <div className='location animate__animated animate__fadeInRight'>
+                                        <h3>Location</h3>
+                                        <p>
+                                            <img src={CountryIcon} alt='globe icon' />
+                                            {userInfo.state}, {userInfo.country}
+                                        </p>
                                     </div>
-                                </div>
-                            </section>
-
-                            <section className='about'>
-                                <div className='animate__animated animate__fadeInUp'>
-                                    <h2>About Me</h2>
-                                    <p>
-                                        My name is {userInfo.firstName} {userInfo.lastName}.
-                                        I&apos;m a(n) {userInfo.occupation} based in {userInfo.state}, {userInfo.country}.
-                                    </p>
-                                    <p>
-                                        I specialise in {userInfo.keySkill}.
-                                        <span className='more'>..</span>
-                                    </p>
-                                    <span>Hire me</span>
-                                </div>
-
-                                <div className='location animate__animated animate__fadeInRight'>
-                                    <h3>Location</h3>
-                                    <p>
-                                        <img src={CountryIcon} alt='globe icon' />
-                                        {userInfo.state}, {userInfo.country}
-                                    </p>
-                                </div>
-                            </section>
+                                </section>
+                            </div>
                         </div>
+                        <Footer />
                     </div>
-                    <Footer />
-                </div>
-            </main>
-    );
+                </main>
+            );
 };
 
 export default User;
