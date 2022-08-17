e51e0b66b9be3cd88adfa2c5fcb041a1b825ad3d
Miracle Ufodiama <miracleufodiama@gmail.com>
2022-08-17 01:34:58 +0100

    chore: heroku deploy config

--- server/app.js
+++ server/app.js
@@ -27,14 +27,14 @@ app.listen(port, () => {
 });
 
 if (
-    process.env.NODE_ENV === 'production' ||
-    process.env.NODE_ENV === 'staging'
-  ) {
-    app.use(express.static(path.join(__dirname, 'client', 'build')));
+  process.env.NODE_ENV === 'production' ||
+  process.env.NODE_ENV === 'staging'
+) {
+  app.use(express.static(path.join(__dirname, '../', 'client', 'build')));
 
-    app.get('*', function (req, res) {
-      res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
-    });
+  app.get('*', function (req, res) {
+    res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
+  });
 }
 
 app.use((req, res, next) => {
