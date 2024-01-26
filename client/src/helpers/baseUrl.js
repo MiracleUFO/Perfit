9f47ebf588c0825f99b135ccbed37deed730b3ad
MiracleUFO <miracle.ufodiama.242949@unn.edu.ng>
2024-01-26 12:45:25 +0100

    chore: migrating from Heroku

--- client/src/helpers/baseUrl.js
+++ client/src/helpers/baseUrl.js
@@ -1,7 +1,7 @@
 import isDev from './isDev';
 
 const baseUrl = () => {
-    const url = isDev() ? 'http://localhost:8080' : '';
+    const url = isDev() ? 'http://localhost:8080' : process.env.BASE_URL;
     return url;
 };
 
